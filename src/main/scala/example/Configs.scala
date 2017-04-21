430dea46eb92b20a06c243197c77a3c4ac87413c
Howard Mao <zhehao.mao@gmail.com>
2017-04-21 10:41:35 -0700

    trying to get DMA working on TileLink2

--- src/main/scala/example/Configs.scala
+++ src/main/scala/example/Configs.scala
@@ -2,6 +2,11 @@ package example
 
 import config.{Parameters, Config}
 import testchipip.WithSerialAdapter
+import coreplex.WithRoCC
+import dma._
+
+class DmaConfig extends Config(
+  new WithDma ++ new WithRoCC ++ new DefaultExampleConfig)
 
 class DefaultExampleConfig extends Config(
   new WithSerialAdapter ++ new rocketchip.DefaultConfig)
